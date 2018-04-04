a7c008c48f01c195f1e983250e7be816f78daa0f
Bogdan Bujdea <bogdan@thewindev.net>
2018-04-04 03:02:15 +0300

    Implemented the analyzer for ordered members

--- MembersSort/MembersSort/MembersSortAnalyzer.cs
+++ MembersSort/MembersSort/MembersSortAnalyzer.cs
@@ -1,11 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
+using System.Net.Http.Headers;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace MembersSort
@@ -28,24 +25,72 @@ namespace MembersSort
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
             context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
         }
 
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            var classSymbol = context.Symbol as INamedTypeSymbol;
+            if (classSymbol?.TypeKind != TypeKind.Class)
+                return; //we only need classes, not other types like enum, struct, etc.
+            var members = GetMembersFromClass(classSymbol); //retrieve only methods and properties
+            var unorderedMember = GetUnorderedMember(members); //then find the first member that is out of order
+            if (unorderedMember != null)
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
+                var rule = new DiagnosticDescriptor(DiagnosticId, Title, $"Move {unorderedMember.Name} lower in the file", Category,
+                    DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+                //now we're creating a rule that tells the user to move the member lower in the file
+                var diagnostic = Diagnostic.Create(rule, unorderedMember.Locations[0], classSymbol);
                 context.ReportDiagnostic(diagnostic);
             }
+        }
+
+        private static ISymbol GetUnorderedMember(List<ISymbol> members)
+        {
+            //I'm just sorting the members by DeclaredAccessibility, then comparing them with the order of
+            //the initial members
+            var orderedByAccessibility = members.OrderByDescending(m => m.DeclaredAccessibility).ToList();
+            for (var i = 0; i < members.Count; i++)
+            {
+                if (members[i].DeclaredAccessibility != orderedByAccessibility[i].DeclaredAccessibility)
+                {
+                    if (orderedByAccessibility[i].DeclaredAccessibility < members[i].DeclaredAccessibility)
+                    {
+                        return members[i];
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static List<ISymbol> GetMembersFromClass(INamedTypeSymbol classSymbol)
+        {
+            var members = new List<ISymbol>();
+            foreach (var member in classSymbol.GetMembers())
+            {
+                if (member.Kind == SymbolKind.Method)
+                {
+                    //I'm not interested in the constructor because it could be private for singletons and on top of the file
+                    //also a method may be public but it's getter or setter may be private so I'm only interested in the property access
+                    var methodMember = member as IMethodSymbol;
+                    if (methodMember.MethodKind == MethodKind.Constructor ||
+                        methodMember.MethodKind == MethodKind.PropertyGet || methodMember.MethodKind == MethodKind.PropertySet)
+                        continue;
+                }
+
+                //same with fields, they usually go on top of the file so we shouldn't ask the
+                //user to move them down
+                if (member.Kind == SymbolKind.Field)
+                {
+                    continue;
+                }
+                //there are other cases probably, but as I said before this is just for demo purposes
+                //and I'm finding them as I use the analyzer with my code
+                members.Add(member);
+            }
+
+            return members;
         }
     }
 }
